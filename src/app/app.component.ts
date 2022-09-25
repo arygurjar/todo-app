f724b56f9dcc5f1f3668cf703e221fa08a8a5608
arungurjar2786 <arygurjar@gmail.com>
2022-09-25 17:55:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-todo';
+  taskInput: any;
+  listOfTask: Array<string> = [];
+
+  addTask() {
+    this.listOfTask.push(this.taskInput);
+    this.taskInput=''
+  }
+
+  removeTask(item: number) {
+    if (item > -1) {
+      this.listOfTask.splice(item, 1);
+    }
+  }
 }
